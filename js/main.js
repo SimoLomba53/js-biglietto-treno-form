8dbacd95782862130e4a552885684658bd27396d
SimoLomba53 <simolomba253@gmail.com>
2023-01-24 15:54:09 +0100

    problemi eventlistener

--- js/main.js
+++ js/main.js
@@ -8,24 +8,31 @@ console.log('Anni:', eta);
 const buttongenera = document.getElementById("genera");
 console.log('Bottone genera', buttongenera);
 //PREZZO SENZA SCONTO
-let pricenosale = (kmnumber * 0.21);
-console.log('Prezzo senza sconto:', pricenosale)
-//SCONTI
-let price = pricenosale;
-
-if (eta < 18) {
-    price = (price - (pricenosale / 100 * 20));
-    console.log(price);
-}
-
-if (eta > 65) {
-    price = (price - (pricenosale / 100 * 40));
-    console.log(price);
-}
 
 
-//IMPORT PRICE IN HTML
-const prezzofinale= `Il tuo prezzo sarà: ${price.toFixed(2)} €`;
-console.log(prezzofinale);
-const prezzofinito = document.getElementById('finalprice');
-prezzofinito.innerHTML = prezzofinale;
+buttongenera.addEventListener{
+    "click"
+    function(click){
+    let pricenosale = (kmnumber * 0.21);
+    console.log('Prezzo senza sconto:', pricenosale)
+    //SCONTI
+    let price = pricenosale;
+
+    if (eta < 18) {
+        price = (price - (pricenosale / 100 * 20));
+        console.log(price);
+    }
+
+    if (eta > 65) {
+        price = (price - (pricenosale / 100 * 40));
+        console.log(price);
+    }
+
+
+    //IMPORT PRICE IN HTML
+    const prezzofinale = `Il tuo prezzo sarà: ${price.toFixed(2)} €`;
+    console.log(prezzofinale);
+    const prezzofinito = document.getElementById('finalprice');
+    prezzofinito.innerHTML = prezzofinale;
+    }
+};
