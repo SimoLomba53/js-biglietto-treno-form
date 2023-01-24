8ef64e6cd6a0586a0d21f83cb13c0c7151875558
SimoLomba53 <simolomba253@gmail.com>
2023-01-24 15:33:12 +0100

    html e java struttura senza css

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+const kmnumber = document.getElementById("km");
+console.log('Totale Kilometri:', kmnumber);
+//ETA'
+const eta = document.getElementById("eta");
+console.log('Anni:', eta);
+//PREZZO SENZA SCONTO
+let pricenosale = (kmnumber * 0.21);
+console.log('Prezzo senza sconto:', pricenosale)
+//SCONTI
+let price = pricenosale;
+
+if (eta < 18) {
+    price = (price - (pricenosale / 100 * 20));
+    console.log(price);
+}
+
+if (eta > 65) {
+    price = (price - (pricenosale / 100 * 40));
+    console.log(price);
+}
+
+
+//IMPORT PRICE IN HTML
+const prezzofinale= `Il tuo prezzo sarà: ${price.toFixed(2)} €`;
+console.log(prezzofinale);
+const prezzofinito = document.getElementById('finalprice');
+prezzofinito.innerHTML = prezzofinale;
